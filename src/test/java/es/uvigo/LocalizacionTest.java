de898e873baa7dc3f95386289e3e3d35753efd0a
Diego Villanueva Vilar <dvillanuevavilar@gmail.com>
2016-11-21 13:28:08 +0100

    Implementados test en localizacion acorde con las relaciones

--- src/test/java/es/uvigo/LocalizacionTest.java
+++ src/test/java/es/uvigo/LocalizacionTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.hibernate.LazyInitializationException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -35,17 +36,25 @@ public class LocalizacionTest extends SQLBasedTest {
 		super.renewConnection();
 	}
 
+	// Create
 	@Test
 	public void testCreateLocalizacion() throws SQLException {
-		final Localizacion loc = new Localizacion();
+		Statement statement = jdbcConnection.createStatement();
+		int id = statement.executeUpdate("INSERT INTO Accidente(fecha) values('2016-01-01')",
+				Statement.RETURN_GENERATED_KEYS);
 
+		final Localizacion loc = new Localizacion();
+		loc.setLocalidad("Orense");
 		doTransaction(emf, em -> {
-			loc.setLocalidad("Orense");
 			em.persist(loc);
+			Accidente a = em.find(Accidente.class, id);
+			a.setLocalizacion(loc);
 		});
 
+		loc.getAccidentes();
+
 		// check
-		Statement statement = jdbcConnection.createStatement();
+		statement = jdbcConnection.createStatement();
 		ResultSet rs = statement.executeQuery("SELECT COUNT(*) as total FROM Localizacion WHERE id = " + loc.getId());
 		rs.next();
 
@@ -53,114 +62,51 @@ public class LocalizacionTest extends SQLBasedTest {
 
 	}
 
+	// Find
 	@Test
-	public void testFindById() throws SQLException {
+	public void testFindLocalizacion() throws SQLException {
 		// prepare database for test
 		Statement statement = jdbcConnection.createStatement();
 		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Orense')",
 				Statement.RETURN_GENERATED_KEYS);
+		int localizacionId = getLastInsertedId(statement);
 
-		int id = getLastInsertedId(statement);
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate("INSERT INTO Accidente(fecha, localizacion_id) values('2016-01-01', " + localizacionId + ")", Statement.RETURN_GENERATED_KEYS);
+		int accidenteId = getLastInsertedId(statement);
 
 		// test code
-		Localizacion loc = emf.createEntityManager().find(Localizacion.class, id);
+		Localizacion loc = emf.createEntityManager().find(Localizacion.class, localizacionId);
 
 		// assert code
 		assertEquals("Orense", loc.getLocalidad());
-		assertEquals(id, loc.getId());
+		assertEquals(1, loc.getAccidentes().size());
+		assertEquals(accidenteId, loc.getAccidentes().iterator().next().getId());
+		assertEquals(loc, loc.getAccidentes().iterator().next().getLocalizacion());
 	}
 
-	// U
-	@Test
-	public void testUpdateLocalizacion() throws SQLException {
-		// prepare database for test
+	private Localizacion detachedLocalizacion = null;
+
+	@Test(expected = LazyInitializationException.class)
+	public void testLazyInitializationException() throws SQLException {
 		Statement statement = jdbcConnection.createStatement();
-		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Vigo')", Statement.RETURN_GENERATED_KEYS);
+		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Orense')", Statement.RETURN_GENERATED_KEYS);
+		int localizacionId = getLastInsertedId(statement);
 
-		int id = getLastInsertedId(statement);
-
-		doTransaction(emf, em -> {
-			Localizacion e = em.find(Localizacion.class, id);
-			e.setLocalidad("Santiago");
-		});
-
-		// check
 		statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT * FROM Localizacion WHERE id = " + id);
-		rs.next();
+		statement.executeUpdate("INSERT INTO Accidente(fecha, localizacion_id) values('2016-01-01', " + localizacionId + ")",
+				Statement.RETURN_GENERATED_KEYS);
+		int accidenteId = getLastInsertedId(statement);
 
-		assertEquals("Santiago", rs.getString("localidad"));
-		assertEquals(id, rs.getInt("id"));
-
-	}
-
-	// U
-	private Localizacion aDetachedLocalizacion = null;
-
-	@Test
-	public void testUpdateByMerge() throws SQLException {
-		// prepare database for test
-		Statement statement = jdbcConnection.createStatement();
-		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Vigo')", Statement.RETURN_GENERATED_KEYS);
-		int id = getLastInsertedId(statement);
-
-		doTransaction(emf, em -> {
-			aDetachedLocalizacion = em.find(Localizacion.class, id);
-		});
-		// e is detached, because the entitymanager em is closed (see
-		// doTransaction)
-
-		aDetachedLocalizacion.setLocalidad("Santiago");
-
-		doTransaction(emf, em -> {
-			em.merge(aDetachedLocalizacion);
-		});
-
-		// check
 		statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT * FROM Localizacion WHERE id = " + id);
-		rs.next();
-
-		assertEquals("Santiago", rs.getString("localidad"));
-		assertEquals(id, rs.getInt("id"));
-	}
-
-	// D
-	@Test
-	public void testDeleteLocalizacion() throws SQLException {
-		// prepare database for test
-		Statement statement = jdbcConnection.createStatement();
-		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Vigo')", Statement.RETURN_GENERATED_KEYS);
-		int id = getLastInsertedId(statement);
+		statement.executeUpdate("INSERT INTO Accidente(fecha, localizacion_id) values('2016-01-01', " + localizacionId + ")",
+				Statement.RETURN_GENERATED_KEYS);
 
 		doTransaction(emf, em -> {
-			Localizacion e = em.find(Localizacion.class, id);
-			em.remove(e);
+			detachedLocalizacion = em.find(Localizacion.class, localizacionId);
 		});
-
-		// check
-		statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT COUNT(*) as total FROM Localizacion WHERE id = " + id);
-		rs.next();
-
-		assertEquals(0, rs.getInt("total"));
-	}
-
-	// L
-	@Test
-	public void testListLocalizacion() throws SQLException {
-		// prepare database for test
-		Statement statement = jdbcConnection.createStatement();
-		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Ávila')", Statement.RETURN_GENERATED_KEYS);
-		// prepare database for test
-		statement.executeUpdate("INSERT INTO Localizacion(localidad) values('Barcelona')", Statement.RETURN_GENERATED_KEYS);
-
-		List<Localizacion> localizaciones = emf.createEntityManager()
-				.createQuery("SELECT loc FROM Localizacion loc ORDER BY loc.localidad", Localizacion.class).getResultList();
-
-		// check
-		//assertEquals(2, localizaciones.size());
-		assertEquals("Ávila", localizaciones.get(0).getLocalidad());
-		assertEquals("Barcelona", localizaciones.get(1).getLocalidad());
+		assertEquals(1, detachedLocalizacion.getAccidentes().size());
+		assertEquals(accidenteId, detachedLocalizacion.getAccidentes().iterator().next().getId());
+		assertEquals(detachedLocalizacion, detachedLocalizacion.getAccidentes().iterator().next().getId());
 	}
 }
