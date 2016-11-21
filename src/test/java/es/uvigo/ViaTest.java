4a58df9f0e767fec7e6026d48629e49d2a1e9ba6
Diego Villanueva Vilar <dvillanuevavilar@gmail.com>
2016-11-21 22:09:55 +0100

    Resolved #5. Tests de via con relaciones

--- src/test/java/es/uvigo/ViaTest.java
+++ src/test/java/es/uvigo/ViaTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.hibernate.LazyInitializationException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -37,37 +38,48 @@ public class ViaTest extends SQLBasedTest {
 
 	@Test
 	public void testCreateVia() throws SQLException {
-		final Via via = new Via();
+		Statement statement = jdbcConnection.createStatement();
+		int id = statement.executeUpdate("INSERT INTO Accidente(fecha) values('2016-01-01')",
+				Statement.RETURN_GENERATED_KEYS);
 
+		final Via via = new Via();
+		via.setEstado_via("Mojado");
+		via.setN_carriles(2);
+		via.setPeligros_calzada("Ninguno");
+		via.setTipo_via("Nacional");
+		via.setUrbano(true);
+		via.setVelocidad_autorizada(80);
 		doTransaction(emf, em -> {
-			via.setEstado_via("Mojado");
-			via.setN_carriles(2);
-			via.setPeligros_calzada("Ninguno");
-			via.setTipo_via("Nacional");
-			via.setUrbano(true);
-			via.setVelocidad_autorizada(80);
 			em.persist(via);
+			Accidente a = em.find(Accidente.class, id);
+			a.setVia(via);
 		});
 
-		Statement statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT COUNT(*) as total FROM Via" + " WHERE id = " + via.getId());
+		via.getAccidentes();
+
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery("SELECT COUNT(*) as total FROM Via WHERE id = " + via.getId());
 		rs.next();
 
 		assertEquals(1, rs.getInt("total"));
 	}
 
 	@Test
-	public void testFindById() throws SQLException {
+	public void testFindVia() throws SQLException {
 		Statement statement = jdbcConnection.createStatement();
 		statement
 				.executeUpdate(
 						"INSERT INTO Via(Estado_via, N_carriles, Peligros_calzada, Tipo_via, Urbano, Velocidad_autorizada)"
 								+ "values ('Mojado', 2, 'Ninguno', 'Nacional', true, 80)",
 						Statement.RETURN_GENERATED_KEYS);
+		int viaId = getLastInsertedId(statement);
 
-		int id = getLastInsertedId(statement);
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate("INSERT INTO Accidente(fecha, via_id) values('2016-01-01', " + viaId + ")",
+				Statement.RETURN_GENERATED_KEYS);
+		int accidenteId = getLastInsertedId(statement);
 
-		Via via = emf.createEntityManager().find(Via.class, id);
+		Via via = emf.createEntityManager().find(Via.class, viaId);
 
 		assertEquals("Mojado", via.getEstado_via());
 		assertEquals(2, via.getN_carriles());
@@ -75,130 +87,40 @@ public class ViaTest extends SQLBasedTest {
 		assertEquals("Nacional", via.getTipo_via());
 		assertEquals(true, via.isUrbano());
 		assertEquals(80, via.getVelocidad_autorizada());
-		assertEquals(id, via.getId());
+		assertEquals(1, via.getAccidentes().size());
+		assertEquals(accidenteId, via.getAccidentes().iterator().next().getId());
+		assertEquals(via, via.getAccidentes().iterator().next().getVia());
 	}
 
-	// Update
-	@Test
-	public void testUpdateVia() throws SQLException {
-		// prepare database for test
+	private Via detachedVia = null;
+
+	@Test(expected = LazyInitializationException.class)
+	public void testLazyInitializationException() throws SQLException {
 		Statement statement = jdbcConnection.createStatement();
 		statement
 				.executeUpdate(
 						"INSERT INTO Via(Estado_via, N_carriles, Peligros_calzada, Tipo_via, Urbano, Velocidad_autorizada)"
 								+ "values ('Mojado', 2, 'Ninguno', 'Nacional', true, 80)",
 						Statement.RETURN_GENERATED_KEYS);
+		int viaId = getLastInsertedId(statement);
 
-		int id = getLastInsertedId(statement);
-
-		doTransaction(emf, em -> {
-			Via via = em.find(Via.class, id);
-			via.setEstado_via("Seco");
-			via.setN_carriles(5);
-			via.setUrbano(false);
-		});
-
-		// check
 		statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT * FROM Via WHERE id = " + id);
-		rs.next();
+		statement.executeUpdate(
+				"INSERT INTO Accidente(fecha, via_id) values('2016-01-01', " + viaId + ")",
+				Statement.RETURN_GENERATED_KEYS);
+		int accidenteId = getLastInsertedId(statement);
 
-		assertEquals("Seco", rs.getString("Estado_via"));
-		assertEquals(5, rs.getInt("N_carriles"));
-		assertEquals(false, rs.getBoolean("Urbano"));
-		assertEquals(id, rs.getInt("id"));
-
-	}
-
-	// Update by merge
-	private Via aDetachedVia = null;
-
-	@Test
-	public void testUpdateByMerge() throws SQLException {
-		// prepare database for test
-		Statement statement = jdbcConnection.createStatement();
-		statement
-				.executeUpdate(
-						"INSERT INTO Via(Estado_via, N_carriles, Peligros_calzada, Tipo_via, Urbano, Velocidad_autorizada)"
-								+ "values ('Mojado', 2, 'Ninguno', 'Nacional', true, 80)",
-						Statement.RETURN_GENERATED_KEYS);
-		int id = getLastInsertedId(statement);
-
-		doTransaction(emf, em -> {
-			aDetachedVia = em.find(Via.class, id);
-		});
-		// e is detached, because the entitymanager em is closed (see
-		// doTransaction)
-
-		aDetachedVia.setEstado_via("Seco");
-		aDetachedVia.setN_carriles(5);
-		aDetachedVia.setUrbano(false);
-
-		doTransaction(emf, em -> {
-			em.merge(aDetachedVia);
-		});
-
-		// check
 		statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT * FROM Via WHERE id = " + id);
-		rs.next();
-
-		assertEquals("Seco", rs.getString("Estado_via"));
-		assertEquals(5, rs.getInt("N_carriles"));
-		assertEquals(false, rs.getBoolean("Urbano"));
-		assertEquals(id, rs.getInt("id"));
-	}
-
-	// Delete
-	@Test
-	public void testDeleteVia() throws SQLException {
-		// prepare database for test
-		Statement statement = jdbcConnection.createStatement();
-		statement
-				.executeUpdate(
-						"INSERT INTO Via(Estado_via, N_carriles, Peligros_calzada, Tipo_via, Urbano, Velocidad_autorizada)"
-								+ "values ('Mojado', 2, 'Ninguno', 'Nacional', true, 80)",
-						Statement.RETURN_GENERATED_KEYS);
-		int id = getLastInsertedId(statement);
+		statement.executeUpdate(
+				"INSERT INTO Accidente(fecha, via_id) values('2016-01-01', " + viaId + ")",
+				Statement.RETURN_GENERATED_KEYS);
 
 		doTransaction(emf, em -> {
-			Via con = em.find(Via.class, id);
-			em.remove(con);
+			detachedVia = em.find(Via.class, viaId);
 		});
-
-		// check
-		statement = jdbcConnection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT COUNT(*) as total FROM Via WHERE id = " + id);
-		rs.next();
-
-		assertEquals(0, rs.getInt("total"));
-	}
-
-	// List
-	@Test
-	public void testListVia() throws SQLException {
-		// prepare database for test
-		Statement statement = jdbcConnection.createStatement();
-		statement
-				.executeUpdate(
-						"INSERT INTO Via(Estado_via, N_carriles, Peligros_calzada, Tipo_via, Urbano, Velocidad_autorizada)"
-								+ "values ('Mojado', 6, 'Ninguno', 'Nacional', true, 90)",
-						Statement.RETURN_GENERATED_KEYS);
-		// prepare database for test
-		statement
-				.executeUpdate(
-						"INSERT INTO Via(Estado_via, N_carriles, Peligros_calzada, Tipo_via, Urbano, Velocidad_autorizada)"
-								+ "values ('Mojado', 7, 'Ninguno', 'Comarcal', true, 85)",
-						Statement.RETURN_GENERATED_KEYS);
-
-		List<Via> vias = emf.createEntityManager()
-				.createQuery("SELECT v FROM Via v ORDER BY v.n_carriles DESC", Via.class)
-				.getResultList();
-
-		// check
-		//assertEquals(2, vias.size());
-		assertEquals(7, vias.get(0).getN_carriles());
-		assertEquals(6, vias.get(1).getN_carriles());
+		assertEquals(1, detachedVia.getAccidentes().size());
+		assertEquals(accidenteId, detachedVia.getAccidentes().iterator().next().getId());
+		assertEquals(detachedVia, detachedVia.getAccidentes().iterator().next().getId());
 	}
 
 }
