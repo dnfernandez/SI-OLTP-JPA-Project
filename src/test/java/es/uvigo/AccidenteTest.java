21f9d4a81afc628ce85d10fcde0dffbadb4b9734
dnfernandez <dnfneira@gmail.com>
2016-11-24 19:40:58 +0100

    Test de accidente_daminificado y accidente_vehiculo resolved #6 resolved #4

--- src/test/java/es/uvigo/AccidenteTest.java
+++ src/test/java/es/uvigo/AccidenteTest.java
@@ -6,7 +6,9 @@ import static org.junit.Assert.assertEquals;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -34,5 +36,70 @@ public class AccidenteTest extends SQLBasedTest {
 		super.renewConnection();
 	}
 
-	
+	@Test
+	public void testFindDamnificadoAccidente() throws SQLException {
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO Damnificado(Edad, Gravedad, Pasajero, Rango_edad, Sexo)"
+						+ "values (24, 'Muerto', 'Delantero-derecha', '20-25', 'Mujer')",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int idDam = getLastInsertedId(statement);
+
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate("INSERT INTO Accidente(fecha) " + "values('2016/11/24')",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int idAc = getLastInsertedId(statement);
+
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate("INSERT INTO Damnificado_Accidente(damnificados_id, accidentes_id) " + "values(" + idDam
+				+ "," + idAc + ")", Statement.RETURN_GENERATED_KEYS);
+
+		EntityManager em = emf.createEntityManager();
+		Damnificado dam = em.find(Damnificado.class, idDam);
+		Accidente ac = em.find(Accidente.class, idAc);
+
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery(
+				"SELECT * FROM Damnificado_Accidente WHERE damnificados_id = " + idDam + " and accidentes_id= " + idAc);
+		rs.next();
+
+		// check
+		assertEquals(idDam, rs.getInt("damnificados_id"));
+		assertEquals(idAc, rs.getInt("accidentes_id"));
+	}
+
+	@Test
+	public void testFindVehiculoAccidente() throws SQLException {
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO Vehiculo(tipo_vehiculo, articulado, anhos, maniobra, impacto, volante_izq, combustible) values('Camión', 'Doble remolque', 25, 'Giro', 'Frontal', true, 'Diesel')",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int idVeh = getLastInsertedId(statement);
+
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate("INSERT INTO Accidente(fecha) " + "values('2016/11/24')",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int idAc = getLastInsertedId(statement);
+
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate("INSERT INTO Vehiculo_Accidente(vehiculos_id, accidentes_id) " + "values(" + idVeh
+				+ "," + idAc + ")", Statement.RETURN_GENERATED_KEYS);
+
+		EntityManager em = emf.createEntityManager();
+		Vehiculo veh = em.find(Vehiculo.class, idVeh);
+		Accidente ac = em.find(Accidente.class, idAc);
+
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery(
+				"SELECT * FROM Vehiculo_Accidente WHERE vehiculos_id = " + idVeh + " and accidentes_id= " + idAc);
+		rs.next();
+
+		// check
+		assertEquals(idVeh, rs.getInt("vehiculos_id"));
+		assertEquals(idAc, rs.getInt("accidentes_id"));
+	}
 }
