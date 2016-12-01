41b56328eb7296080659dfeb70869db4b7b17f78
Diego Neira <dnfneira@gmail.com>
2016-12-01 12:24:53 +0100

    Añadido doTransaction en TransactionUtils

--- src/main/java/es/uvigo/TransactionUtils.java
+++ src/main/java/es/uvigo/TransactionUtils.java
@@ -4,6 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
 public class TransactionUtils {
 
 	public static void doTransaction(EntityManagerFactory emf, Transaction transaction) {
@@ -13,16 +17,22 @@ public class TransactionUtils {
 			tx = em.getTransaction();
 			try {
 				tx.begin();
-				transaction.run(em);
+					transaction.run(em);
 				tx.commit();
-			} finally {
-				if (tx != null && tx.isActive()) {
-					tx.rollback();
-				}
+			} finally{
+				if (tx !=null && tx.isActive()){ tx.rollback(); }
 			}
-		} finally {
-			em.close();
-		}
+		} finally{	em.close(); 	}
 	}
 
+	public static void doTransaction(EntityManager em, Transaction transaction) {
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+				transaction.run(em);
+			tx.commit();
+		} finally {
+			if (tx !=null && tx.isActive()){ tx.rollback(); }
+		}
+	}
 }
